24b78a30095ed085dac6d22eaad87e14ecaab87e
Tom Seymour <tomeseymour44@gmail.com>
2017-06-27 09:29:54 -0400

    length of last word

--- DSA.playground/Pages/June 2017.xcplaygroundpage/Contents.swift
+++ DSA.playground/Pages/June 2017.xcplaygroundpage/Contents.swift
@@ -53,3 +53,38 @@ func canPlaceFlowers(_ flowerbed: [Int], _ n: Int) -> Bool {
 }
 
 canPlaceFlowers([0], 2)
+
+
+
+
+/*
+ Given a string s consists of upper/lower-case alphabets and empty space characters ' ', return the length of last word in the string.
+ 
+ If the last word does not exist, return 0.
+ 
+ Note: A word is defined as a character sequence consists of non-space characters only.
+ 
+ For example,
+ Given s = "Hello World",
+ return 5.
+ 
+*/
+
+func lengthOfLastWord(_ s: String) -> Int {
+    guard s.characters.count > 0 else { return 0 }
+    
+    var arr = s.components(separatedBy: CharacterSet.whitespaces)
+
+    while arr.last!.characters.count == 0 {
+        _ = arr.popLast()
+        if arr.isEmpty {
+            return 0
+        }
+    }
+    
+    return arr.last!.characters.count
+    
+}
+
+
+lengthOfLastWord("  ")
